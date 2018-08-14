88ad8de811b1000f681d23617102d277378074cc
cartacode <cartacode@outlook.com>
2018-08-14 18:31:28 +0530

    BOOTSTRAP UI Issue first load css error fixed..... Install bootstrap@3.3.4 react plugin for that and remove cdna bootstrap path

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
@@ -6,6 +7,7 @@ import App from 'components/App';
 import configureStore from 'store/configureStore';
 
 import 'styles/main.scss';
+
 import 'react-notifications/lib/notifications.css';
 
 render(
