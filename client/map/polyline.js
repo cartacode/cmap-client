79bea1ef52ee6c01de4b18e6ba8cb2584e811a86
cartacode <cartacode@outlook.com>
2018-08-23 02:26:33 -0400

    Corrects polyline map utility to interface with Cesium viewer instances;

--- client/map/polyline.js
+++ client/map/polyline.js
@@ -1,75 +1,51 @@
 import Cesium from 'cesium/Cesium'; // eslint-disable-line import/no-unresolved
 import uuid from 'uuid/v4';
 
+import { viewers } from 'map/viewer';
 import { getRandomColor, getRGBArray } from 'util/color';
 
 /**
- * The map of polyline collections.
- * @static
- * @type  {Map}
- */
-const polylineCollections = new Map();
-
-/**
- * Adds a polyline to the collection corresponding to the provided identifier.
+ * Adds a polyline to the viewer corresponding to the provided identifier.
  * @param   {Object}          config              The configuration object.
- * @param   {string}          config.collectionId The identifier of the polyline collection.
  * @param   {string}          [config.color]      The color of the polyline represented as a hex string.
- * @param   {Array.<Object>}  config.points       The coordinate points of the polyline.
+ * @param   {string}          [config.id]         The identifier of the polyline.
+ * @param   {Array.<number>}  config.points       The coordinate points of the polyline.
+ * @param   {string}          config.viewerId     The identifier of the representative viewer.
  * @param   {number}          [config.width]      The width of the polyline.
  * @returns {Object}
  */
-export function addPolyline({ collectionId, color = getRandomColor(), points, width = 2 }) {
-  let collection;
-
-  if (polylineCollections.has(collectionId)) {
-    collection = polylineCollections.get(collectionId);
-  } else {
-    collection = new Cesium.PolylineCollection();
-    polylineCollections.set(collectionId, collection);
+export function addPolyline({ color = getRandomColor(), id = `Polyline__${uuid()}`, points, viewerId, width = 2 }) {
+  if (!viewers.has(viewerId)) {
+    return;
   }
 
-  return collection.add({
-    id: `Polyline__${uuid()}`,
-    material: new Cesium.Material({
-      fabric: {
-        type: 'Color',
-        uniforms: {
-          color: new Cesium.Color(...getRGBArray(color), 1.0),
+  return viewers.get(viewerId).entities.add({
+    polyline: {
+      id,
+      material: new Cesium.Material({
+        fabric: {
+          type: 'Color',
+          uniforms: {
+            color: new Cesium.Color(...getRGBArray(color), 1.0),
+          },
         },
-      },
-    }),
-    positions: Cesium.Cartesian3.fromDegreesArray(points),
-    width,
+      }),
+      positions: Cesium.Cartesian3.fromDegreesArray(points),
+      width,
+    },
   });
 }
 
 /**
- * Removes the polyline from the collection corresponding to the provided identifier.
- * @param {string}  collectionId  The identifier of the polyline collection.
- * @param {Object}  polyline      The polyline to remove.
+ * Removes the polyline from the viewer corresponding to the provided identifier.
+ * @param   {string}  viewerId  The identifier of the representative viewer.
+ * @param   {string}  polyline  The polyline to remove.
+ * @returns {boolean} Indicates whether the polyline was removed.
  */
-export function removePolyline(collectionId, polyline) {
-  if (polylineCollections.has(collectionId)) {
-    polylineCollections.get(collectionId).remove(polyline);
-  }
-
-  polyline.destroy();
-}
-
-/**
- * Removes the polyline collection corresponding to the provided identifier.
- * @param {string}  collectionId  The identifier of the polyline collection.
- */
-export function removePolylineCollection(collectionId) {
-  if (!polylineCollections.has(collectionId)) {
+export function removePolyline(viewerId, polyline) {
+  if (!viewers.has(viewerId)) {
     return;
   }
 
-  const collection = polylineCollections.get(collectionId);
-
-  polylineCollections.delete(collectionId);
-
-  collection.removeAll();
-  collection.destroy();
+  return viewers.get(viewerId).entities.remove(polyline);
 }
