ef3bf31bd76bcb08d8d7482eda32e738ab8677f1
cartacode <cartacode@outlook.com>
2018-08-23 16:28:15 +0530

    Bug #9752 resolved.

--- client/components/admin/location/BaseModal.js
+++ client/components/admin/location/BaseModal.js
@@ -340,8 +340,8 @@ class BaseModal extends React.Component {
 
     const locationFields = [
       { name: translations['LocationType'], type: 'dropdown', domID: 'LocationType', ddID: 'LocationCategory', valFieldID: 'LocationCategory' },
-      { name: translations['Lat'], type: 'number', domID: 'LocationLat', valFieldID: 'LocationLatitude' },
-      { name: translations['Lon'], type: 'number', domID: 'LocationLon', valFieldID: 'LocationLongitude' },
+      { name: translations['Lat'], type: 'number', domID: 'LocationLat', valFieldID: 'LocationLatitude', isDecimal: true },
+      { name: translations['Lon'], type: 'number', domID: 'LocationLon', valFieldID: 'LocationLongitude', isDecimal: true },
       { name: translations['Elevation'], type: 'number', domID: 'LocationElevation', valFieldID: 'LocationElevation' },
       { name: translations['MGRS'], type: 'input', domID: 'LocationMGRS', valFieldID: 'LocationMGRS' },
       { name: translations['LocationID'], type: 'input', domID: 'LocationID', ddID: '', valFieldID: 'UserLocationID', required: true, validationIcon: true },
--- client/components/reusable/ContentBlock.js
+++ client/components/reusable/ContentBlock.js
@@ -165,9 +165,17 @@ class ContentBlock extends React.Component {
             minValue = item.minValue;
           }
           if (item.required) {
-            input = (<input type="number" min={minValue} value={value} className="form-control" name={item.valFieldID} onChange={this.handleChangeNumber} required />);
+            if(item.isDecimal){
+              input = (<input type="number" step="any" min={minValue} value={value} className="form-control" name={item.valFieldID} onChange={this.handleChangeNumber} required />);
+            }else {
+              input = (<input type="number" min={minValue} value={value} className="form-control" name={item.valFieldID} onChange={this.handleChangeNumber} required />);
+            }
           } else {
-            input = (<input type="number" min={minValue} value={value} className="form-control" name={item.valFieldID} onChange={this.handleChangeNumber} />);
+            if(item.isDecimal){
+              input = (<input type="number" step="any" min={minValue} value={value} className="form-control" name={item.valFieldID} onChange={this.handleChangeNumber} />);
+            }else {
+              input = (<input type="number" min={minValue} value={value} className="form-control" name={item.valFieldID} onChange={this.handleChangeNumber} />);
+            }
           }
           break;
 
