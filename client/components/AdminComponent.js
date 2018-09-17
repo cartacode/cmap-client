afb3f2b4af78b9b508012d6f8d961750bd2a3f91
cartacode <cartacode@outlook.com>
2018-09-17 16:30:13 +0100

    Removed ComNet Sys Health

--- client/components/AdminComponent.js
+++ client/components/AdminComponent.js
@@ -52,8 +52,8 @@ class AdminComponent extends React.Component {
       {title: translations['Location'], url: `${match.url}/location`},
       {title: translations['Ccir/Pir'], url: `${match.url}/ccir-pir`},
       {title: translations['Org builder'], url: `${match.url}/org-builder`},
-      {title: translations['Com/Net'], url: `${match.url}/com-net/satcom`},
-      {title: translations['Sys. Health'], url: `${match.url}/sys-health`},
+      // {title: translations['Com/Net'], url: `${match.url}/com-net/satcom`},
+      // {title: translations['Sys. Health'], url: `${match.url}/sys-health`},
       {title: 'Reference Docs', url: `${match.url}/sys-config`},
     ];
 
