ba8354bf94d97b4586361d23802236e7c304a6c6
cartacode <cartacode@outlook.com>
2018-10-05 15:35:14 +0530

    9898 changes for revert the code for these changes in PLatform inventory

--- client/components/admin/platform/AddPlatformInventory.js
+++ client/components/admin/platform/AddPlatformInventory.js
@@ -295,7 +295,8 @@ updateDeployedUnits= (branch, unit) => {
 
     const generalFields = [
       { name: "Platform Specifications", type: 'dropdown', ddID: 'Platform/GetPlatforms', domID: 'metaDataID', valFieldID: 'metaDataID', required: true },
-      { name: translations['Tail#'], type: 'input', domID: 'Tail#', valFieldID: 'tailNumber', required: true, regexType:'Alphanumeric', regex: '^[a-zA-Z0-9]+$' },
+      //{ name: translations['Tail#'], type: 'input', domID: 'Tail#', valFieldID: 'tailNumber', required: true, regexType:'Alphanumeric', regex: '^[a-zA-Z0-9]+$' },
+      { name: translations['Tail#'], type: 'input', domID: 'Tail#', valFieldID: 'tailNumber', required: true },
       { name: translations['COCOM'], type: 'dropdown', domID: 'dispLocationCOCOM', ddID: 'COCOM',valFieldID: 'COCOM'},
       { name: translations['Contract Company'], type: 'input', domID: 'Company', valFieldID: 'Company', required: true },
       { name: translations['Branch'], type: 'dropdown', domID: 'ServiceBranch', ddID: 'BranchOfService', valFieldID: 'branch', required: true },
