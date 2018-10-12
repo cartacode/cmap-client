e4dcc52313a4083694eaf576c0c9f63846285d3b
cartacode <cartacode@outlook.com>
2018-10-12 18:03:03 +0530

    video streaming player is integrated for the time being we are passing static URL for live streaming.

--- client/components/reusable/OperationVideoBlock.js
+++ client/components/reusable/OperationVideoBlock.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import {connect} from 'react-redux';
+import ReactPlayer from 'react-player';
 
 
 class OperationVideoBlock extends React.Component {
@@ -17,42 +18,42 @@ class OperationVideoBlock extends React.Component {
 
     let borderBottom, progressbar, width, video;
 
-      switch(this.props.blockHeader) {
-        case translations['blue devil']:
-          borderBottom = '2px solid #09a3c3';
-          width = this.props.percent;
-          progressbar = '/assets/img/dashboard/blue_progressbar.png';
-          video = '/assets/img/dashboard/video1.png';
-          break;
+    switch(this.props.blockHeader) {
+      case translations['blue devil']:
+        borderBottom = '2px solid #09a3c3';
+        width = this.props.percent;
+        progressbar = '/assets/img/dashboard/blue_progressbar.png';
+        video = '/assets/img/dashboard/video1.png';
+        break;
 
-        case translations['valient angel']:
-          borderBottom = '2px solid #ff00ff';
-          width = this.props.percent;
-          progressbar = "/assets/img/dashboard/pink_progressbar.png";
-          video = '/assets/img/dashboard/video2.png';
-          break;
+      case translations['valient angel']:
+        borderBottom = '2px solid #ff00ff';
+        width = this.props.percent;
+        progressbar = "/assets/img/dashboard/pink_progressbar.png";
+        video = '/assets/img/dashboard/video2.png';
+        break;
 
-        case translations['rolling thunder']:
-          borderBottom = '2px solid #ff0000';
-          width = this.props.percent;
-          progressbar = '/assets/img/dashboard/red_progressbar.png';
-          video = "/assets/img/dashboard/video3.png";
-          break;
+      case translations['rolling thunder']:
+        borderBottom = '2px solid #ff0000';
+        width = this.props.percent;
+        progressbar = '/assets/img/dashboard/red_progressbar.png';
+        video = "/assets/img/dashboard/video3.png";
+        break;
 
-        case translations['she devil']:
-          borderBottom = '2px solid #00a651';
-          width = this.props.percent;
-          progressbar = "/assets/img/dashboard/green_progressbar.png";
-          video = "/assets/img/dashboard/video3.png";
-          break;
+      case translations['she devil']:
+        borderBottom = '2px solid #00a651';
+        width = this.props.percent;
+        progressbar = "/assets/img/dashboard/green_progressbar.png";
+        video = "/assets/img/dashboard/video3.png";
+        break;
           
-        default:
-          borderBottom = '2px solid #00a651';
-          width = this.props.percent;
-          progressbar = "/assets/img/dashboard/green_progressbar.png";
-          video = "/assets/img/dashboard/video3.png";
-          break;  
-      }
+      default:
+        borderBottom = '2px solid #00a651';
+        width = this.props.percent;
+        progressbar = "/assets/img/dashboard/green_progressbar.png";
+        video = "/assets/img/dashboard/video3.png";
+        break;  
+    }
 
     return (
       <div className="col-md-3 map-block">
@@ -76,10 +77,17 @@ class OperationVideoBlock extends React.Component {
           </div>
           <div className="col-md-12">
             <div className="progress-bar">
-              <img src={progressbar} alt="" style={{width}} />
+              <img src={progressbar} alt="" style={{width}} /> 
             </div>
           </div>
-          <img src={video} className="video" alt=""/>
+          {/* <img src={video} className="video" alt=""/> */}
+          <ReactPlayer
+            url='https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8'
+            className='video'
+            playing
+            width='100%'
+            height='100%'
+          />
         </div>
       </div>
     );
