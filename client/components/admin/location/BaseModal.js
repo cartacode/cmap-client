386eb74185b947718cd260e2ffcf549d8e651632
cartacode <cartacode@outlook.com>
2018-10-23 17:20:08 +0530

    Map Uncommented

--- client/components/admin/location/BaseModal.js
+++ client/components/admin/location/BaseModal.js
@@ -442,7 +442,7 @@ class BaseModal extends React.Component {
          </div>
          <div className = "row personnel">
             <div className="col-md-12">
-             Map size="100" viewerId={viewerIdentifiers.location} updateLatLong={this.updateLatLong} />
+            <Map size="100" viewerId={viewerIdentifiers.location} updateLatLong={this.updateLatLong} />
            </div>
          </div>
          <div className="row personnel" >
