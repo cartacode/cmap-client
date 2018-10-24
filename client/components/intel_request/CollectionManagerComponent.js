a4a172ea5eeaee8403b272cc8034d735c3a456ec
cartacode <cartacode@outlook.com>
2018-10-24 16:21:46 +0530

    add class bt-xs in summary pages

--- client/components/intel_request/CollectionManagerComponent.js
+++ client/components/intel_request/CollectionManagerComponent.js
@@ -211,10 +211,10 @@ class CollectionManagerComponent extends React.Component {
         Cell: row => (
           <div>
             {/* <Link to={`${editurl}${row.value}`} className="text-success"  title="Edit" > <span className="glyphicon glyphicon-edit" /> </Link> */}&nbsp;
-            <a href="Javascript: void('0');" className="btn btn-primary" title={translations["Move To Collection Plan"]} onClick={() => this.moveToCollectionPlan(row)} > <span className="glyphicon glyphicon-circle-arrow-right" /></a>
+            <a href="Javascript: void('0');" className="btn btn-primary btn-xs" title={translations["Move To Collection Plan"]} onClick={() => this.moveToCollectionPlan(row)} > <span className="glyphicon glyphicon-circle-arrow-right" /></a>
             
             &nbsp;
-            <a href="JavaScript: void('0');" className="btn btn-danger" title={translations["Delete"]} onClick={() => this.deleteApprovedIntelRequests(row.value)} ><span className="glyphicon glyphicon-trash" /> </a>
+            <a href="JavaScript: void('0');" className="btn btn-danger btn-xs" title={translations["Delete"]} onClick={() => this.deleteApprovedIntelRequests(row.value)} ><span className="glyphicon glyphicon-trash" /> </a>
           </div>
         ),
       },
@@ -274,7 +274,7 @@ class CollectionManagerComponent extends React.Component {
         maxWidth: 100,
         Cell: row => (
           <div>
-            <a href="Javascript:void('0');" className="btn btn-primary" title={translations["Move To Intel Request"]} onClick={() => this.moveToIntelRequest(row.value)} > <span className="glyphicon glyphicon-circle-arrow-left" /> </a>
+            <a href="Javascript:void('0');" className="btn btn-primary btn-xs" title={translations["Move To Intel Request"]} onClick={() => this.moveToIntelRequest(row.value)} > <span className="glyphicon glyphicon-circle-arrow-left" /> </a>
                        &nbsp;
           </div>
         ),
