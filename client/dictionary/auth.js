a29f3b72c1e19fce008319316cf9686df2c72ae0
cartacode <cartacode@outlook.com>
2018-11-22 03:45:51 +0530

    ISR Sync Users

--- client/dictionary/auth.js
+++ client/dictionary/auth.js
@@ -57,3 +57,10 @@ export const superAdmin = ["f5bfb78f-38c6-48f4-8773-59c2543e0805"];
 export const sysDocsUser = ["586f2a2d-ded0-43cc-879f-a0ab98b567a8"];
 
 export const intelCustomer = ['0425a61c-f00e-40d7-93d4-f811e1891ade'];
+
+export const missionISRUser = ["b63ca868-af4b-486f-9540-3d01eca089fe","7554b13b-bb2b-4203-a334-5417f5b03650",
+                                "d669b1ed-5e2c-4c27-8d16-60388dffcc2a","6dd5ba55-f445-4a30-8270-fdbc992434af", 
+                                "0425a61c-f00e-40d7-93d4-f811e1891ade", "f09c4e3a-eebe-434e-a2a2-b0b44cbcca93", 
+                                "36c9952b-3f63-468d-9335-3294a91e9c97", "d3afa700-751d-4f08-b99e-f9a15361c7a1",
+                                "7637f5c8-aa0c-4447-b7a9-939aadd07c7a", "af036592-2598-4727-bc36-7f71c665f787",
+                                "f5bfb78f-38c6-48f4-8773-59c2543e0805"];
