41a01b4daeff6c62653435a21148998b43bb984f
cartacode <cartacode@outlook.com>
2019-03-25 16:50:15 +0530

    Detail position changes in Personel

--- client/components/live_view/SideLeft/PersonnelPopup/PersonnelPopupComponent.js
+++ client/components/live_view/SideLeft/PersonnelPopup/PersonnelPopupComponent.js
@@ -104,7 +104,7 @@ class PersonnelPopupComponent extends React.Component {
                                 Number(item.longitude) === 0 ? -77.009003 : Number(item.longitude),
                                 'people_logo', '', item.ID, '', '<img src="/assets/img/admin/photo_1_old.png" style="height:62%;float:left;margin-left:-10px;margin-right:5px;">' + item.firstName + ' ' + item.lastName +
                                 '<br/>' + item.rank + '<br/>' + item.branchOfService + ' / ' + item.assignedUnit + '<br/>' + item.branchOfService + ' / ' + item.deployedUnit + '<br/>' +
-                                item.location + '<br/>' + item.status + '<a style="position: absolute; right: 0px; color: #b8850c;"><strong>Details</strong></a>', false);
+                                item.location + '<br/>' + item.status + '<a style="position: absolute; right: 15px; color: #b8850c;"><strong>Details</strong></a>', false);
 
             return <PlatformPopupItemComponent
               color={'#FFA500'}
