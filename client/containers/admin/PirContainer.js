17135fa7b041e5049db7c7e4e9f91fbe1d879176
cartacode <cartacode@outlook.com>
2019-06-28 13:01:01 +0530

    changes for Admin Screens

--- client/containers/admin/PirContainer.js
+++ client/containers/admin/PirContainer.js
@@ -1,18 +1,21 @@
 import {connect} from 'react-redux';
 
 import PirComponent from '../../components/admin/PirComponent';
+import {fetchCcirPirs, deleteCcirPirById} from 'actions/ccirpir';
 
 const mapStateToProps = state => {
   return {
     translations: state.localization.staticText,
-   
+    allCcirPirs: state.ccirpir.allCcirPirs,
+    isDeleted: state.ccirpir.isDeleted
   };
 };
 
-const mapDispatchToProps = {
- 
-};
 
+const mapDispatchToProps = {
+  fetchCcirPirs, 
+  deleteCcirPirById
+};
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(PirComponent);
